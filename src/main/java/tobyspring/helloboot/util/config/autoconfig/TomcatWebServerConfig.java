ca90f5e81c37b3cd28b44a5aabf738da2ea719af
khmgobe <dr94406@naver.com>
2024-07-16 18:12:01 +0900

    feat: 섹션 7, 조건부 자동 구성 파트 실습 완료.

--- src/main/java/tobyspring/helloboot/util/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/helloboot/util/config/autoconfig/TomcatWebServerConfig.java
@@ -1,14 +1,18 @@
 package tobyspring.helloboot.util.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import tobyspring.helloboot.util.config.ConditionalMyOnClass;
 import tobyspring.helloboot.util.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Bean
+    @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletContainer() {
         return new TomcatServletWebServerFactory();
     }
